83f67de6648d42b291be74155738e43d7a729f34
k00278910 <k00278910@student.lit.ie>
2023-11-20 16:23:05 +0000

    adds select location container and dropdown

--- js/index.js
+++ js/index.js
@@ -10,3 +10,30 @@ console.log(navlinks);
 // class nav-links to show-nav-links 
 navlinks.classList.toggle('show-nav-links');
 }
+
+  function selectedLocation() {
+    var selectElement = document.getElementById("country");
+    var selectedOption = selectElement.options[selectElement.selectedIndex].value; 
+    var imageElement = document.getElementById("flagImage");
+    
+    switch (selectedOption) {
+        
+        case 'spain':
+            console.log(selectedOption);
+            imageElement.src = "images/spain.png";       
+            break;
+           
+        case 'germany':
+            console.log(selectedOption);
+            imageElement.src = "images/germany.png";                
+            break;
+                   
+        case 'italy':
+            console.log(selectedOption);
+            imageElement.src = "images/italy.png";     
+            break;
+            
+        default:
+            console.log('Not valid option');  
+    }
+  }
